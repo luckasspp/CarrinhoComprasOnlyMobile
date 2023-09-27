20c1a095044ab6cd0cf986ccd474846847a7db68
luckasspp <eng.luckas@gmail.com>
2023-09-27 20:32:58 -0300

    Update banco.js

--- php/banco.js
+++ php/banco.js
@@ -1,4 +1,4 @@
-produtos = [{nome: 'Amburguer', src: 'img/IMG01.JPG', preco: 19.99 },
+produtos = [{nome: 'Hamburguer', src: 'img/IMG01.JPG', preco: 19.99 },
             {nome: 'Bauru', src: 'img/IMG02.JPG', preco: 19.99 },
             {nome: 'Bolo chocolate', src: 'img/IMG03.JPG', preco: 39.99 },
             {nome: 'Sonho', src: 'img/IMG04.JPG', preco: 9.99 },
